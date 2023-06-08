a4510ddf7a74f1ee4764e7ca850e832f4a06e20c
Lipee221 <115747668+Lipee221@users.noreply.github.com>
2023-06-08 15:54:26 -0300

    Snack - Stock Atualizados.
    Snack e Stock atualizados.

--- src/controllers/SnackController.java
+++ src/controllers/SnackController.java
@@ -7,6 +7,13 @@ package controllers;
 import dao.IngredientDAO;
 import dao.SnackDAO;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.OK_CANCEL_OPTION;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 import models.Ingredient;
@@ -18,10 +25,29 @@ import models.Snack;
  */
 public class SnackController {
     
-     public void readJTable(javax.swing.JTable table) throws SQLException{
+    private JTable table;
+    private List<JTextField> fields;
+    
+    public void setJTable(JTable table){
+        this.table = table;
+    }
+    public void setFields(JTextField field){
+        this.fields.add(field);
+    }
+    public JTable getTable(){
+        return this.table;
+    }
+    public List<JTextField> getFields(){
+        return this.fields;
+    }
+    
+    
+    
+    
+     public void readJTable() throws SQLException{
         
-        DefaultTableModel modelo = (DefaultTableModel) table.getModel();        
-        table.setRowSorter(new TableRowSorter(modelo));
+        DefaultTableModel modelo = (DefaultTableModel) this.table.getModel();        
+        this.table.setRowSorter(new TableRowSorter(modelo));
         modelo.setNumRows(0);
         
         SnackDAO dao = new SnackDAO();
@@ -38,50 +64,96 @@ public class SnackController {
         }       
     }
     
-    public void readJTableBusca(String search,javax.swing.JTable table ) throws SQLException{
+    public void readJTableSearch(String search) throws SQLException{
         
-        DefaultTableModel modelo = (DefaultTableModel) table.getModel();        
-        table.setRowSorter(new TableRowSorter(modelo));
-        modelo.setNumRows(0);
+        DefaultTableModel model = (DefaultTableModel) this.table.getModel();        
+        this.table.setRowSorter(new TableRowSorter(model));
+        model.setNumRows(0);
         
         SnackDAO dao = new SnackDAO();
         
         for (Snack snack: dao.search(search)){
-            modelo.addRow(new Object[]{
+            model.addRow(new Object[]{
                 snack.getId(),
                 snack.getSnackTitle(),
                 snack.getSnackSellingPrice(),
                 snack.getSnackDescription(),
                 snack.getSnackImageUrl(),
                 snack.getSnackStatus(),
-            
-                 });
-             
+               
+            });
         }       
     }
 
-    public void limpar(){
-        
-        //limpar os campos
-        txtId.setText("");
-        txtDescricao.setText("");
-        txtValor.setText("");
-        txtBusca.setText("");
-        txtDescricao.grabFocus();
-        
-        btnAdicionar.setEnabled(true);
-        txtBusca.setEnabled(true);
-        btnBuscar.setEnabled(true);
-        
-        btnExcluir.setEnabled(false);
-        btnAtualizar.setEnabled(false);
-        
+    public void clean (List <javax.swing.JTextField> fields){
+        fields.forEach((field) -> {
+                field.setText("");
+        });
         try {
-            readJTable();
+            this.readJTable();
+            
         } catch (SQLException ex) {
             System.out.println("Erro ao acessar o Banco de dador" + ex);
         }
-        
     }
+
+    public boolean add (List <javax.swing.JTextField> fields) throws SQLException{
+        boolean isEmpty = false;
+        for(int i = 0; i > fields.size(); i++){
+            if(fields.get(i).getText().isEmpty()){
+                isEmpty = true;
+                break;
+            }
+        }
+        if(!isEmpty){
+            return false;
+        } else{
+            Snack snack = new Snack();
+            SnackDAO dao = new SnackDAO();
+            
+            snack.setSnackTitle(fields.get(0).getText());
+            snack.setSnackSellingPrice(Float.parseFloat(fields.get(1).getText()));
+            snack.setSnackDescription(fields.get(2).getText());
+            snack.setSnackImageUrl(fields.get(3).getText());
+            snack.setSnackStatus(fields.get(4).getText());
+            
+            try {
+                dao.addSnack(snack);
+                return true;
+            } catch (SQLException ex) {
+                System.out.print(ex);
+                return false;
+            }
+        }
+    }
+    
+
+     
+    public void delete(int id) throws SQLException{
+        if (this.table.getSelectedRow() != -1){
+            int answer = JOptionPane.showConfirmDialog(null,
+                    "Confirma a Exclusão do Registro?", 
+                    "Exclusão de Registro",OK_CANCEL_OPTION);
+            if(answer == 0){
+                Snack snack = new Snack();
+                SnackDAO dao = new SnackDAO();                
+                snack.setId((int) this.table.getValueAt(
+                        this.table.getSelectedRow(), 0));
+                
+                try {
+                    dao.deleteSnack(snack.getId());
+                } catch (SQLException ex) {
+                    System.out.print(ex);
+                }
+                
+               this.readJTable();
+            }
+        }
+        else{
+            JOptionPane.showMessageDialog(null,
+                    "Selecione um serviço na tabela abaixo!");
+        }
+    }
+    
 }
 
