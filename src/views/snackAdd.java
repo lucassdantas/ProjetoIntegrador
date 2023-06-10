bf2619d1e9a238b35c1eb262c6023d341e1cc5e3
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-06-09 21:26:26 -0300

    snack add edit and delete working

--- src/views/snackAdd.java
+++ src/views/snackAdd.java
@@ -15,13 +15,13 @@ import javax.swing.JTable;
  *
  * @author Work.Studies
  */
-public class snackAdd extends javax.swing.JFrame {
+public class SnackAdd extends javax.swing.JFrame {
 
     /**
      * Creates new form EstoqueEntrada
      */
     private final SnackController snackController;
-    public snackAdd() {
+    public SnackAdd() {
         initComponents();
         this.snackController = new SnackController();
     }
@@ -251,14 +251,22 @@ public class snackAdd extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(snackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SnackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(snackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SnackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(snackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SnackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(snackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SnackAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -271,7 +279,7 @@ public class snackAdd extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new snackAdd().setVisible(true);
+                new SnackAdd().setVisible(true);
             }
         });
     }
